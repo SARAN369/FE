6318da244677fd37bb93840ff84303de3fc83ef4
saranjothi55 <saran13dr@gmail.com>
2025-04-24 09:06:29 +0530

    first commit

---
+++ react-inscribe.tsx
@@ -0,0 +1,21 @@
+/**
+ * necessitate
+ *  assets
+ *  common
+ *  config
+ *  features
+ *  hooks
+ *  redux
+ *  utils
+ * 
+ * 
+ * framework-build
+ *  hoc => higher order components
+ *  atom and molecules
+ *  story book
+ *  CLS
+ *  FCP
+ *  long pooling
+ *  debouncing
+ *  redux toolkit
+ */
---
+++ script.js
@@ -0,0 +1,5 @@
+// interpolation of string adding
+
+var namea = "guru";
+
+console.log(`Hello from name is : ${namea}` )
